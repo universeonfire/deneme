ed32dc5b1eaa393727664641dd4bbd1c83fb8783
Evren <evren@Evren-iMac.local>
2018-10-12 09:59:56 +0300

    seeds change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |x|
+	Blog.create!(
+		title: "Blog Kaydı #{x}",
+		body: "At vero eos et accusamus et iusto odio dignissimos ducimus qui 
+		blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et 
+		quas molestias excepturi sint occaecati cupiditate non provident, 
+		similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. 
+		Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, 
+		cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, 
+		omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis 
+		debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. 
+		Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus 
+		maiores alias consequatur aut perferendis doloribus asperiores repellat."	
+	)
+end
+5.times do |x|
+	Skill.create!(
+		title: "Rails #{x}",
+		percent_utilizzed: 10
+	)
+end
+9.times do |x|
+	Portfolio.create!(
+		title: "bişeyler #{x}",
+		subtitle: "iki şeyler #{x}",
+		body:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+		sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+		Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+		image:"https://via.placeholder.com/600x400",
+		thumb_mage:"https://via.placeholder.com/350x150"
+	)
+end
