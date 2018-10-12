f5b8c45d7da21d5d5149767efb8c9677cdd6efb5
Evren <evren@Evren-iMac.local>
2018-10-12 11:50:51 +0300

    düzenleme

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -8,13 +8,28 @@ class PortfoliosController < ApplicationController
 		@portfolio_item = Portfolio.new
 	end
 
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
 	def create
 		@portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title,:subtitle, :body))
 		respond_to do |format|
 			if @portfolio_item.save
 				format.html{redirect_to portfolios_path, notice: 'port oluşturuldu'}
 			else
-				format.html{render:new}
+				format.html{render :new}
+			end
+		end
+	end
+
+	def update
+		@portfolio_item = Portfolio.find(params[:id])
+		respond_to do |format|
+			if @portfolio_item.update(params.require(:portfolio).permit(:title,:subtitle, :body))
+				format.html{redirect_to portfolios_path, notice: 'Port düzenlendi'}
+			else
+				format.html{render :edit}
 			end
 		end
 	end
