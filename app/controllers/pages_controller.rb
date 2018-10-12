6b6973e1de9e5e81cc401e5b2c6d16ad32c0c172
Evren <evren@Evren-iMac.local>
2018-10-12 10:36:55 +0300

    doğru seeds

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def home
   	@posts = Blog.all
   	@skills = Skill.all
+  	@portfolio_items = Portfolio.all
   end
 
   def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -37,7 +37,7 @@ end
 		Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
 		Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 
-		image:"https://via.placeholder.com/600x400",
-		thumb_mage:"https://via.placeholder.com/350x150"
+		main_image:"https://via.placeholder.com/600x400",
+		thumb_image:"https://via.placeholder.com/350x150"
 	)
 end
