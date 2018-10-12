2fd0f4b18a80521d382a6bddc818dd8a78650e73
Evren <evren@Evren-iMac.local>
2018-10-12 17:19:45 +0300

    yarım kalan toggle

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -61,6 +61,10 @@ class BlogsController < ApplicationController
     end
   end
 
+  def toggle_status
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,18 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 
-  resources :blogs
+  resources :blogs do
+  	member do
+  		get :toggle_status
+
+  	end
+	end
+
 
   get 'about' , to: 'pages#about'
   get 'contact' , to: 'pages#contact'
-  
  
+  
   # ana sayfa
   root to: 'pages#home'
 end
