b8bf59ea6feb9d3cbea3803252ba5cde8876aa89
Evren <evren@Evren-iMac.local>
2018-10-15 15:21:03 +0300

    default değer tanımlama

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,6 +6,16 @@ class Portfolio < ApplicationRecord
 	end
 	#custom query type 2
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on rails')}
-
 	
+
+	#sabit değer tanımlama
+	#after_initialize controller daosyasındaki new aksiyonu calıstıktan sonra işlemi yapar
+	after_initialize :set_defaults
+	#||=  eğer değer tanımlıysa işlem yapmaz yoksa işlem yapar if main_image == nil 
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/350x150"
+
+	end
+
 end
