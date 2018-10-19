3abdc647f1c50c8951d5120747564ac12d9bff0e
Evren <evren@Evren-iMac.local>
2018-10-19 15:33:38 +0300

    devise controller ına alan ekleme

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	#devise controller ına ulaşabilmek için
+	#configure_permitted_parameters isim onemli değil başka bişey de yazılabilir
+	before_action :configure_permitted_parameters, if: :devise_controller?
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+	end
 end
