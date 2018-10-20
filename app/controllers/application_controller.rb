29cf5eb96dfee4083f1b3fb0dc0be55c738853fd
Evren <evren@Evren-iMac.local>
2018-10-20 13:18:23 +0300

    session çalışması

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 	#concern klasorunun içindeki DeviseWhitelist metodunu alır
 	include DeviseWhitelist
+
+	before_action :set_source
+
+	def set_source
+		session[:source] = params[:q] if params[:q]
+	end
 	
 	
 end
