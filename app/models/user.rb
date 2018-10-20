a5f402cbcfb912c81e92114c6027f3a650951f2e
Evren <evren@Evren-iMac.local>
2018-10-20 11:24:46 +0300

    sanal değişken oluşturma

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #name alanını gerekli hale getiriyoruz ki oluşturacağımız sanal değişkenlere atanacak verileri elde edebilelim       
+  validates_presence_of :name
+
+  def first_name
+  	#self.name.split[0] da kullanılabilir
+  	self.name.split.first
+  end
+
+  def last_name
+  	self.name.split.last
+  end
 end
