bd04d5d91bfab5c449dd05f0a122fd3523e031eb
Evren <evren@Evren-iMac.local>
2018-10-31 12:28:19 +0300

    home page değiştirildi

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,15 +3,15 @@ module ApplicationHelper
 		#örnek bir helper metodu html_safe komutu bu helper ın kullanıldığı yerde string i html kodu olarak algılanmasını sağlar
 		"<p> my Helper </p>".html_safe
 	end
-	def login_helper
+	def login_helper(style)
     	if current_user.is_a?(GuestUser)
             #ruby bu tür komutlarda son itemi gösterir bunun olmaması için parantez içine almamız gerekli ve + simgesiyle birleştirilmeli string concat mantığı işte
-            (link_to "Sing Up", new_user_registration_path) + 
-            "<br>".html_safe + 
-            (link_to "Sign In",  new_user_session_path)
+            (link_to "Sing Up", new_user_registration_path, class: style) + 
+            " ".html_safe + 
+            (link_to "Sign In",  new_user_session_path, class: style)
     		
     	else
-    		link_to "Logout", destroy_user_session_path, method: :delete 
+    		link_to "Logout", destroy_user_session_path, method: :delete , class: style
     	end
 
 	end
