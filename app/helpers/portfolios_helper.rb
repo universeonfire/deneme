3282abdf51e4525d6cee6ce1df81096939e800a3
Evren <evren@Evren-iMac.local>
2018-11-09 15:03:39 +0300

    Placeholder düzeltildi dosya yükleme tamamlandı

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -3,15 +3,16 @@ module PortfoliosHelper
 		"https://via.placeholder.com/#{height}x#{width}"
 	end
 
-	#def portfolio_img img, type
-		#if img.model.main_image? || img.model.thumb_image?
-			#img
-		#elsif type == 'thumb'
-			#image_generator(height: '350', width: '150')
-		#elsif type == 'main'
-			#image_generator(height: '600', width: '400')
-		#end
+	def portfolio_img img, type
+		
+		if img && img != ''
+			img
+		elsif type == 'thumb'
+			image_generator(height: '350', width: '150')
+		else 
+			image_generator(height: '600', width: '400')
+		end
 		 
-	#end
+	end
 
 end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,28 +4,19 @@ class Portfolio < ApplicationRecord
 	accepts_nested_attributes_for :technologies, 
 								   reject_if: lambda {|attrs| attrs['name'].blank?}
 
-	include Placeholder
-	validates_presence_of :title, :body , :main_image, :thumb_image
+	 
+	validates_presence_of :title, :body 
 
 	mount_uploader :thumb_image, PortfolioUploader
 	mount_uploader :main_image, PortfolioUploader
-	# custom query type 1
+	 
 	def self.angular
 		where(subtitle: 'angular')
 	end
-	#custom query type 2
+	 
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: 'Ruby on rails')}
 	
-
-	#sabit değer tanımlama
-	#after_initialize controller daosyasındaki new aksiyonu calıstıktan sonra işlemi yapar
-	after_initialize :set_defaults
-	#||=  eğer değer tanımlıysa işlem yapmaz yoksa işlem yapar if main_image == nil 
-	def set_defaults
-		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-		self.thumb_image ||= Placeholder.image_generator(height:'350',width:'150')
-
-	end
+ 
 	def self.by_pos
 		self.order("position ASC")
 	end
