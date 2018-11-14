d6b9178d91b431b4ae74a3794ba4b31f1167729e
Evren <evren@Evren-iMac.local>
2018-11-14 11:23:03 +0300

    actioncable yapısı eklendi

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  belongs_to :user
+  belongs_to :user 
   belongs_to :blog
   validates :content, presence: true, length: {minimum: 5, maximum: 1000}
   #jobs/CommentBroadcastJob
