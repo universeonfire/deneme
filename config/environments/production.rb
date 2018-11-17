e9a0715562ec54c381ec490b5f074f93088d3afd
Evren <evren@Evren-iMac.local>
2018-11-17 15:24:14 +0300

    action cable ayarı

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://en-portfoliosite.herokuapp.com','http://en-portfoliosite.herokuapp.com']
+  config.action_cable.url = "wss://en-portfoliosite.herokuapp.com/cable"
 end
